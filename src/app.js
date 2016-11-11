d39007a4db6d8596710971c286c998850af4a4aa
tranquangvu <vutran.513@gmail.com>
2016-11-12 03:00:50 +0700

    simple socket implement

--- src/app.js
+++ src/app.js
@@ -1,7 +1,32 @@
 import scheduler from './scheduler';
+import io from 'socket.io-client';
+import { getValueOfDevice } from './services/device';
 
 // cron job scheduler
-scheduler();
-console.log('-> Cron job is running...');
+// scheduler();
+// console.log('-> Cron job is running...');
 
 // action cable
+const socket = io('http://0.0.0.0:5001');
+
+socket.on('connect', () => {
+  console.log('-> Connected');
+});
+
+socket.on('get_current_value', (data) => {
+  let { device } = data
+
+  getValueOfDevice(device)
+    .then((values) => {
+      socket.emit('response_current_value', values);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+socket.on('disconnect', () => {
+  console.log('-> Disconnected');
+});
+
+console.log('Listening on socket.....');
